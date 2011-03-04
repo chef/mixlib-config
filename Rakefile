fa42def234f3a2d69229340733131b93a887be8d
Daniel DeLeo <dan@opscode.com>
2011-03-04 08:41:33 -0800

    upgrade to rspec 2 and rm debug output from tests

--- Rakefile
+++ Rakefile
@@ -17,25 +17,10 @@ rescue LoadError
    puts "Jeweler (or a dependency) not available. Install from gemcutter with: sudo gem install gemcutter jeweler"
 end
 
-require 'spec/rake/spectask'
-Spec::Rake::SpecTask.new(:spec) do |spec|
-  spec.libs << 'lib' << 'spec'
-  spec.spec_opts = ['--options', "\"#{File.dirname(__FILE__)}/spec/spec.opts\""]
-  spec.spec_files = FileList['spec/**/*_spec.rb']
-end
-
-Spec::Rake::SpecTask.new(:rcov) do |spec|
-  spec.libs << 'lib' << 'spec'
-  spec.pattern = 'spec/**/*_spec.rb'
-  spec.rcov = true
-end
-
 begin
-  require 'cucumber/rake/task'
-  Cucumber::Rake::Task.new(:features)
-rescue LoadError
-  task :features do
-    abort "Cucumber is not available. In order to run features, you must: sudo gem install cucumber"
+  require 'rspec/core/rake_task'
+  RSpec::Core::RakeTask.new(:spec) do |spec|
+    spec.pattern = 'spec/**/*_spec.rb'
   end
 end
 
--- spec/mixlib/config_spec.rb
+++ spec/mixlib/config_spec.rb
@@ -6,9 +6,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,24 +30,24 @@ describe Mixlib::Config do
       c[:foo] = nil
     end
   end
-  
+
   it "should load a config file" do
     File.stub!(:exists?).and_return(true)
     File.stub!(:readable?).and_return(true)
     IO.stub!(:read).with('config.rb').and_return("alpha = 'omega'\nfoo = 'bar'")
-    lambda { 
+    lambda {
       ConfigIt.from_file('config.rb')
     }.should_not raise_error
   end
-  
+
   it "should not raise an ArgumentError with an explanation if you try and set a non-existent variable" do
-    lambda { 
+    lambda {
       ConfigIt[:foobar] = "blah"
     }.should_not raise_error(ArgumentError)
   end
-  
+
   it "should raise an Errno::ENOENT if it can't find the file" do
-    lambda { 
+    lambda {
       ConfigIt.from_file("/tmp/timmytimmytimmy")
     }.should raise_error(Errno::ENOENT)
   end
@@ -58,11 +58,11 @@ describe Mixlib::Config do
       ConfigIt.from_file('config.rb')
     }.should raise_error(SyntaxError)
   end
-  
+
   it "should allow you to reference a value by index" do
     ConfigIt[:alpha].should == 'omega'
   end
-  
+
   it "should allow you to set a value by index" do
     ConfigIt[:alpha] = "one"
     ConfigIt[:alpha].should == "one"
@@ -73,12 +73,12 @@ describe Mixlib::Config do
     ConfigIt.arbitrary_value.should == 50
     ConfigIt[:arbitrary_value].should == 50
   end
-  
+
   describe "when a block has been used to set config values" do
     before do
       ConfigIt.configure { |c| c[:cookbook_path] = "monkey_rabbit"; c[:otherthing] = "boo" }
     end
-    
+
     {:cookbook_path => "monkey_rabbit", :otherthing => "boo"}.each do |k,v|
       it "should allow you to retrieve the config value for #{k} via []" do
         ConfigIt[k].should == v
@@ -88,17 +88,17 @@ describe Mixlib::Config do
       end
     end
   end
-  
+
   it "should not raise an ArgumentError if you access a config option that does not exist" do
     lambda { ConfigIt[:snob_hobbery] }.should_not raise_error(ArgumentError)
   end
-  
+
   it "should return true or false with has_key?" do
     ConfigIt.has_key?(:monkey).should eql(false)
     ConfigIt[:monkey] = "gotcha"
     ConfigIt.has_key?(:monkey).should eql(true)
   end
-  
+
   describe "when a class method override accessor exists" do
     before do
       @klass = Class.new
@@ -107,7 +107,6 @@ describe Mixlib::Config do
         config_attr_writer :test_method do |blah|
           blah.is_a?(Integer) ? blah * 1000 : blah
         end
-        pp self.methods
       EVAL
     end
 
@@ -131,11 +130,11 @@ describe Mixlib::Config do
       @klass.from_file('config.rb')
       @klass.test_method.should == 99000
     end
-    
+
     it "should receive internal_set with the method name and config value" do
       @klass.should_receive(:internal_set).with(:test_method, 53).and_return(true)
       @klass[:test_method] = 53
     end
-    
+
   end
 end
