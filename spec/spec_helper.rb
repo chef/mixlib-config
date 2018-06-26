5b051d7055ae82e5c45651076a5e219846e8505c
Lamont Granquist <lamont@scriptkiddie.org>
2018-06-26 13:05:15 -0700

    remove hashrocket syntax
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ class ConfigIt
 end
 
 RSpec.configure do |config|
-  config.filter_run :focus => true
+  config.filter_run focus: true
   config.run_all_when_everything_filtered = true
   config.warnings = true
 end
